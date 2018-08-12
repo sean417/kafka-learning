90faeda8f86be29503b60dca60f95cda5ced5dd5
sean417 <373202226@qq.com>
2018-08-12 13:45:20 +0800

    create test module

---
+++ clients/src/main/java/AA.java
@@ -0,0 +1,8 @@
+/*
+ @author:   chenyang
+ @date  2018/8/12 下午1:44
+
+*/
+
+public class AA {
+}
---
+++ clients/src/main/java/com/kafka/learning/test/clients/AA.java
@@ -0,0 +1,10 @@
+package com.kafka.learning.test.clients;
+
+/*
+ @author:   chenyang
+ @date  2018/8/12 下午1:45
+
+*/
+
+public class AA {
+}
